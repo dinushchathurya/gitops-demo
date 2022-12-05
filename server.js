fbff725190c40d57079fc1880bc9e28d8d4cc445
Dinush Chathurya <dinushchathurya21@gmail.com>
2022-12-05 16:44:19 +0530

    feat: Change request's output text
    Signed-off-by: Dinush Chathurya <dinushchathurya21@gmail.com>

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World Develop\n');
+  res.send('Hello World\n');
 });
 
 app.listen(PORT, HOST);

939363391dd27426e7bbc16fb3b4df0c5a18a76a
Dinush Chathurya <dinushchathurya21@gmail.com>
2022-12-29 07:53:23 +0530

    feat: Change output string

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World\n');
+  res.send('Hello World Production\n');
 });
 
 app.listen(PORT, HOST);
